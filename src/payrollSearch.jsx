fc09f329f80f2cfceebf983f60a551e13e5e1c62
AidanMurray <iiiaiid4niii@gmail.com>
2025-03-09 21:32:04 +0000

    payroll search now pulls from ajson file and uses this to populate the drop down, just need to fill in the json file now

--- src/payrollSearch.jsx
+++ src/payrollSearch.jsx
@@ -1,16 +1,84 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logoImage from "./SoundPound.png";
+import employerLookupFile from "./employerLookup.json";
+import CashboxLogo from "./assets/logos/CashboxLogo.png";
+import HootLogo from "./assets/logos/HootLogo.png";
+import ManchesterLogo from "./assets/logos/ManchesterLogo.png";
+import OldhamLogo from "./assets/logos/OldhamLogo.png";
+import SalfordLogo from "./assets/logos/SalfordLogo.png";
+import SouthManchesterLogo from "./assets/logos/SouthManchesterLogo.png";
+import StockportLogo from "./assets/logos/StockportLogo.png";
+import UnifyLogo from "./assets/logos/UnifyLogo.png";
+
 
 function PayrollSearch() {
+
+  const [payrollPartners, setPayrollPartners] = useState([])
   const handleEmployerChange = () => {};
 
   const handleSubmit = () => {};
 
-  const payrollPartners = [
-    { name: "Payroll1", id: 1 },
-    { name: "Payroll2", id: 2 },
-  ];
+
+
+const converterFunc = (obj) => {
+  const newEmployers = Object.keys(obj).map((employer) => ({
+    name: employer,
+    creditUnions: obj[employer],
+  }));
+
+  setPayrollPartners((prevState) => [...prevState, ...newEmployers]);
+};
+
+  const creditUnionDataTable = {
+      Stockport: {
+        name: "Stockport Credit Union",
+        img_url: StockportLogo,
+        ApplyLink: "https://www.stockportcu.com/what-we-do/annual-bee-bus-ticket/?",
+      },
+      Manchester: {
+        name: "Manchester Credit Union",
+        img_url: ManchesterLogo,
+        ApplyLink: "https://manchestercreditunion.co.uk/products/loans/bee-network-annual-bus-ticket",
+      },
+      Unify: {
+        name: "Unify Credit Union",
+        img_url: UnifyLogo,
+        ApplyLink: "https://unifycu.org/Annual_Bee_Bus_Ticket",
+      },
+      Oldham: {
+        name: "Oldham Credit Union",
+        img_url: OldhamLogo,
+        ApplyLink: "https://www.oldhamcreditunion.co.uk/join/",
+      },
+      "South Manchester": {
+        name: "South Manchester Credit Union",
+        img_url: SouthManchesterLogo,
+        ApplyLink: "https://www.smcreditunion.co.uk/annual-bee-bus-ticket/",
+      },
+      Hoot: {
+        name: "Hoot Credit Union",
+        img_url: HootLogo,
+        ApplyLink: "https://wisewithmoney.org.uk/loans/bee-bus-pass-loan/",
+      },
+      CashBox: {
+        name: "Cash Box Credit Union",
+        img_url: CashboxLogo,
+        ApplyLink: "https://www.cashbox.org.uk/loans/bee-bus-annual-pass-loan",
+      },
+      Salford: {
+        name: "Salford Credit Union",
+        img_url: SalfordLogo,
+        ApplyLink:
+          "https://www.salfordcreditunion.com/annual-bee-bus-ticket-loan/",
+      },
+    };
+
+    useEffect(() => {
+      converterFunc(employerLookupFile)
+      console.log(payrollPartners)
+    },[])
+
 
   return (
     <div className="payrollSearch">
